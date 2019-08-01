2b4e63f4504846ece201530b26cbd609984bdf93
Britain <britainmackenzie@gmail.com>
2019-08-01 15:59:24 -0500

    Update WeatherUnitTest.java

--- app/src/test/java/com/example/weatherapp/WeatherUnitTest.java
+++ app/src/test/java/com/example/weatherapp/WeatherUnitTest.java
@@ -37,9 +37,64 @@ public class WeatherUnitTest{
     }
 
     @Test
-    public void link_check(){
-        String dummyLink = "300@isntIt.gov";
-        weatherTest.setLink("300@isntIt.gov");
-        assertEquals(weatherTest.getLink(),dummyLink);
+    public void temp_check(){
+        String dummyTemp = "200";
+        weatherTest.setTemp("200");
+        assertEquals(Integer.parseInt(weatherTest.getTemp()),Integer.parseInt(dummyTemp));
+    }
+    @Test
+    public void sunrise_check(){
+        String dummySunrise = "200";
+        weatherTest.setSunrise("200");
+        assertEquals(Integer.parseInt(weatherTest.getSunrise()),Integer.parseInt(dummySunrise));
+    }
+    @Test
+    public void sunset_check(){
+        String dummySunset = "200";
+        weatherTest.setSunset("200");
+        assertEquals(Integer.parseInt(weatherTest.getSunset()),Integer.parseInt(dummySunset));
+    }
+    @Test
+    public void unit_check(){
+        String dummyUnit = "200";
+        weatherTest.setUnit("200");
+        assertEquals(Integer.parseInt(weatherTest.getUnit()),Integer.parseInt(dummyUnit));
+    }
+    @Test
+    public void precip_type_check(){
+        String dummyPrecipType = "200";
+        weatherTest.setPrecipType("200");
+        assertEquals(Integer.parseInt(weatherTest.getPrecipType()),Integer.parseInt(dummyPrecipType));
+    }
+    @Test
+    public void precip_prob_check(){
+        int dummyPrecipProb = 200;
+        weatherTest.setPrecipitationProb(200);
+        assertEquals(weatherTest.getPrecipitationProb(),dummyPrecipProb);
+    }
+    @Test
+    public void day_check(){
+        boolean dummyDay = true;
+        weatherTest.setDay(true);
+        assertEquals(weatherTest.getDay(),dummyDay);
+    }
+    @Test
+    public void precip_check(){
+        boolean dummyPrecip = true;
+        weatherTest.setPrecipitation(true);
+        assertEquals(weatherTest.getPrecipitation(),dummyPrecip);
+    }
+    @Test
+    public void icon_check(){
+        String dummyIcon = "200";
+        weatherTest.setIcon("200");
+        assertEquals(Integer.parseInt(weatherTest.getIcon()),Integer.parseInt(dummyIcon));
+    }
+
+    @Test
+    public void icon_phrase_check(){
+        String dummyIconPhrase = "200";
+        weatherTest.setIconPhrase("200");
+        assertEquals(Integer.parseInt(weatherTest.getIconPhrase()),Integer.parseInt(dummyIconPhrase));
     }
 }
