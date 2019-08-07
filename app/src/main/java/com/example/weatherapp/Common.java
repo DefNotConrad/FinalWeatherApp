812d6d17a1a893a489acd100e25859306e5dccb6
quantumtx <q.williams096@gmail.com>
2019-08-07 08:27:05 -0500

    Add files via upload

--- app/src/main/java/com/example/weatherapp/Common.java
+++ app/src/main/java/com/example/weatherapp/Common.java
@@ -6,19 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Common {
-    public static String unixTimeStampToDateTime(double unixTimeStamp){
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        Date date = new Date();
-        date.setTime((long)unixTimeStamp*1000);
-        return dateFormat.format(date);
-    }
 
-    public static String getImage(String icon){
-        return String.format("http://openweathermap.org/img/w/%s.png,icon");
-    }
 
     public static String getDateNow(String newDate){
-        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ss-HH:mm");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss-HH:mm");
         Date date = null;
 
         try {
@@ -27,7 +18,7 @@ public class Common {
             e.printStackTrace();
         }
 
-        DateFormat out = new SimpleDateFormat("MM-dd-YYYY");
+        DateFormat out = new SimpleDateFormat("MM-dd-yyyy");
         return out.format(date);
 
     }
---
+++ app/src/main/java/com/example/weatherapp/ListAdapter.java
@@ -0,0 +1,48 @@
+package com.example.weatherapp;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+public class ListAdapter extends ArrayAdapter<Weather>{
+
+    public ListAdapter(@NonNull Context context, ArrayList<Weather> weatherArrayList) {
+        super(context, 0, weatherArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Weather weather = getItem(position);
+        int key = weather.getKey();
+
+        String current = "Temp: ";
+        String degree = "°";
+        String currentText = current + weather.getTemp() + degree;
+
+        /**
+         * Adds hourly values to listView
+         */
+        if(weather.getKey()==1) {
+            for (int x = 0; x < 12; x++) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_current_hourly, parent, false);
+                TextView dateTextView = convertView.findViewById(R.id.hourly_title);
+                //ImageView iconView = convertView.findViewById(R.id.item_icon);
+                TextView tempView = convertView.findViewById(R.id.current_temp);
+                TextView weatherText = convertView.findViewById(R.id.weather_text);
+                tempView.setText(currentText);
+                weatherText.setText(weather.getIconPhrase());
+                dateTextView.setText(weather.getDate());
+                //iconView.setImageDrawable();
+            }
+        }
+        return convertView;
+    }
+}
