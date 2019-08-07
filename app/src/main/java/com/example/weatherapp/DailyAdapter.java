fce4a833a0ed24cb691aac560a96f9b44ada0526
Britain <britainmackenzie@gmail.com>
2019-08-07 13:58:56 -0500

    Update DailyAdapter.java

--- app/src/main/java/com/example/weatherapp/DailyAdapter.java
+++ app/src/main/java/com/example/weatherapp/DailyAdapter.java
@@ -2,21 +2,46 @@ package com.example.weatherapp;
 
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
+
+/**
+ *
+ * DailyAdapter adapts an array of daily Weather objects into a ListView
+ *
+ */
 public class DailyAdapter extends ArrayAdapter<Weather>{
 
+    /**
+     * Constructor for DailyAdapter utilizes
+     * inheritance from ArrayAdapter class for specified ArrayList
+     *
+     * @param context
+     * @param weatherArrayList ArrayList of Weather objects holding hourly data
+     */
     public DailyAdapter(@NonNull Context context, ArrayList<Weather> weatherArrayList) {
         super(context, 0, weatherArrayList);
     }
 
+    /**
+     *
+     * Adds one Weather item to the ListView at time,
+     * inflates the layout and fills with daily weather information
+     *
+     * @param position position in ArrayList
+     * @param convertView View object
+     * @param parent ViewGroup
+     * @return convertView
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -27,7 +52,7 @@ public class DailyAdapter extends ArrayAdapter<Weather>{
         String degree = "°";
         String minTemp = low + weather.getMinTemp() + degree;
         String maxTemp = high + weather.getMaxTemp() + degree;
-        
+
         convertView = LayoutInflater.from(getContext()).inflate(R.layout.recycler_item, parent, false);
 
             for (int x = 0; x < 5; x++) {
@@ -41,6 +66,13 @@ public class DailyAdapter extends ArrayAdapter<Weather>{
                 maxTextView.setText(maxTemp);
                 precipView.setText(weather.getIconPhrase());
                 //iconView.setImageDrawable();
+
+                ImageView weatherIcon = convertView.findViewById(R.id.daily_icon);
+                Resources resources = convertView.getResources();
+                final int resourceId = resources.getIdentifier("ic_"+weather.getIcon(), "drawable", getContext().getPackageName());
+
+                Drawable myDrawable = convertView.getResources().getDrawable(resourceId);
+                weatherIcon.setImageDrawable(myDrawable);
             }
 
         return convertView;
