ca3c3a0139337ff4bb3bed858e69191c06bfee57
Britain <britainmackenzie@gmail.com>
2019-08-07 04:18:34 -0500

    Update ListAdapter.java
    List adapter for hourly adapting

--- app/src/main/java/com/example/weatherapp/ListAdapter.java
+++ app/src/main/java/com/example/weatherapp/ListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp;
 
+
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,69 +8,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 public class ListAdapter extends ArrayAdapter<Weather>{
 
-        public ListAdapter(@NonNull Context context, ArrayList<Weather> weatherArrayList) {
-            super(context, 0, weatherArrayList);
-        }
+    public ListAdapter(@NonNull Context context, ArrayList<Weather> weatherArrayList) {
+        super(context, 0, weatherArrayList);
+    }
 
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            Weather weather = getItem(position);
-            int key = weather.getKey();
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Weather weather = getItem(position);
+        int key = weather.getKey();
+        
+        String current = "Temp: ";
+        String degree = "°";
+        String currentText = current + weather.getTemp() + degree;
 
-            String precipitation = "Precipitation: ";
-            String precip = "Precip: ";
-            String high = "High: ";
-            String low = "Low: ";
-            String current = "Currently: ";
-            String percent = "%";
-            String degree = "°";
-
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.recycler_item, parent, false);
+        /**
+         * Adds hourly values to listView
+         */
+        if(weather.getKey()==1) {
+            for (int x = 0; x < 12; x++) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_current_hourly, parent, false);
+                TextView dateTextView = convertView.findViewById(R.id.hourly_title);
+                //ImageView iconView = convertView.findViewById(R.id.item_icon);
+                TextView tempView = convertView.findViewById(R.id.current_temp);
+                TextView weatherText = convertView.findViewById(R.id.weather_text);
+                tempView.setText(currentText);
+                weatherText.setText(weather.getIconPhrase());
+                dateTextView.setText(weather.getDate());
+                //iconView.setImageDrawable();
             }
-
-            switch (key) {
-                case 2: {
-                    TextView dateTextView = convertView.findViewById(R.id.current_date);
-                    TextView tempTextView = convertView.findViewById(R.id.current_temp);
-                    TextView precipView = convertView.findViewById(R.id.precipitation_info);
-                    dateTextView.setText(weather.getDate());
-                    tempTextView.setText(current + weather.getTemp() + degree);
-                    precipView.setText(precipitation + weather.getPrecipitationProb() + percent);
-                }
-                case 1: {
-                    TextView dateTextView = convertView.findViewById(R.id.item_top_text);
-                    ImageView iconView = convertView.findViewById(R.id.item_icon);
-                    TextView minTextView = convertView.findViewById(R.id.item_low);
-                    TextView maxTextView = convertView.findViewById(R.id.item_high);
-                    TextView precipView = convertView.findViewById(R.id.item_precip);
-                    dateTextView.setText(weather.getDate());
-                    minTextView.setText(low + weather.getMinTemp() + degree);
-                    maxTextView.setText(high + weather.getMaxTemp() + degree);
-                    precipView.setText(precip + weather.getPrecipitationProb() + percent);
-                    //iconView.setImageDrawable();
-                }
-                case 0: {
-                    TextView dateTextView = convertView.findViewById(R.id.item_top_text);
-                    ImageView iconView = convertView.findViewById(R.id.item_icon);
-                    TextView minTextView = convertView.findViewById(R.id.item_low);
-                    TextView maxTextView = convertView.findViewById(R.id.item_high);
-                    TextView precipView = convertView.findViewById(R.id.item_precip);
-                    dateTextView.setText(weather.getDate());
-                    minTextView.setText(low + weather.getMinTemp() + degree);
-                    maxTextView.setText(high + weather.getMaxTemp() + degree);
-                    precipView.setText(precip + weather.getPrecipitationProb() + percent);
-                    //iconView.setImageDrawable();
-                }
-            }
-                return convertView;
         }
+        return convertView;
+    }
 }
-
